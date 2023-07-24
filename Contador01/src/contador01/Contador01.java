81dcff09583c450ec516ebf4a8c0a2aa93d72beb
Eduardo Pugliesi <70285858+edupugliesi@users.noreply.github.com>
2023-07-24 13:45:46 -0300

    Aula11 + exercícios

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contador01;
+
+/**
+ *
+ * @author edu-pop
+ */
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int cc = 1;
+        while (true) {
+            System.out.println("Cambalhota " + cc);              
+            
+            if (cc == 5){
+                System.out.println("Fim");
+                break;                
+            }
+            
+            cc++;
+             
+            
+        }
+    }
+    
+}
